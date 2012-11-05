dbdfc4e97cedf82fea125358b0b1ea7e71430483
point <alex.softx@gmail.com>
2012-11-05 18:29:44 +0200

    Configure s3 properly.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,11 +13,11 @@ CarrierWave.configure do |config|
     config.storage          = :fog
     config.fog_credentials  = {
       :provider                 => 'AWS',
-      :aws_access_key_id        => "e.musienko@gmail.com", #ENV['S3_KEY_ID'],
-      :aws_secret_access_key    => "turum69burum", #ENV['S3_SECRET_KEY'],
-      :region                   => ENV['S3_BUCKET_REGION']
+      :aws_access_key_id        => "AKIAJQ4SF2VONM7BZEDQ", #ENV['S3_KEY_ID'],
+      :aws_secret_access_key    => "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", #ENV['S3_SECRET_KEY'],
+      :region                   => "eu-west-1", #ENV['S3_BUCKET_REGION']
     }
-    config.fog_directory    = ENV['S3_BUCKET']
+    config.fog_directory    = "new-turum"#ENV['S3_BUCKET']
 
   else
     # settings for the local filesystem
