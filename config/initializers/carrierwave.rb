460779f594a8bc3ed8ba97fcbc8256bd6b8a328f
point <alex.softx@gmail.com>
2012-11-05 17:52:55 +0200

    S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,8 +13,8 @@ CarrierWave.configure do |config|
     config.storage          = :fog
     config.fog_credentials  = {
       :provider                 => 'AWS',
-      :aws_access_key_id        => ENV['S3_KEY_ID'],
-      :aws_secret_access_key    => ENV['S3_SECRET_KEY'],
+      :aws_access_key_id        => "e.musienko@gmail.com", #ENV['S3_KEY_ID'],
+      :aws_secret_access_key    => "turum69burum", #ENV['S3_SECRET_KEY'],
       :region                   => ENV['S3_BUCKET_REGION']
     }
     config.fog_directory    = ENV['S3_BUCKET']
