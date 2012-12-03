4ab29ca6b86a9c80aa9591d85560e564073b52da
point <alex.softx@gmail.com>
2012-12-04 00:01:27 +0200

    Set context.

--- lib/liquid/filters/custom_layout.rb
+++ lib/liquid/filters/custom_layout.rb
@@ -3,8 +3,8 @@ module Liquid
     module CustomLayout
 
       def custom_layout(input, *args)
-        options = args_to_options(args)
-        ::Liquid::Template.parse(input, {}).render
+        #options = args_to_options(args)
+        ::Liquid::Template.parse(input).render(@context.clone)
       end
     end
     ::Liquid::Template.register_filter(CustomLayout)
