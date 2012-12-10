15fedeb3c5cd238205cd220c3f73d944754d2aeb
point <alex.softx@gmail.com>
2012-12-11 01:20:31 +0200

    Precompile options.

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,6 @@ module Myapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompile += %w( application.js  )
   end
 end
