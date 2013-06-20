3967ce575a227d7053b1dbe34fb7f5b424c1235b
point <alex.softx@gmail.com>
2013-06-20 11:55:36 +0300

    Hack to make collections work.

--- config/initializers/liquid_tags.rb
+++ config/initializers/liquid_tags.rb
@@ -1,3 +1,11 @@
 require 'liquid/tags/for_with_random'
 require 'liquid/tags/two_columns'
 require 'liquid/filters/custom_layout'
+
+module Liquid
+  class For < Block
+    def slice_collection_using_each(collection, from, to)
+      Utils.slice_collection_using_each(collection, from, to)
+    end
+  end
+end
