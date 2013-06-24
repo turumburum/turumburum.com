48f432b057b1210a71d242578e018bb30a7fadd4
point <alex.softx@gmail.com>
2013-06-24 11:17:26 +0300

    Monkey patch for current dragonfly

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,5 +1,19 @@
 require 'dragonfly'
-
+module ImageMagick
+  module Utils
+    def identify(temp_object)
+      # example of details string:
+      # myimage.png PNG 200x100 200x100+0+0 8-bit DirectClass 31.2kb
+      format, width, height, depth = raw_identify(temp_object).scan(/([A-Z0-9]+) (\d+)x(\d+) .* (\d+)-bit/)[0]
+      {
+        :format => format.downcase.to_sym,
+        :width => width.to_i,
+        :height => height.to_i,
+        :depth => depth.to_i
+      }
+    end
+  end
+end
 ## initialize Dragonfly ##
 
 app = Dragonfly[:images]
