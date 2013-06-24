a015ac4441f89c2b383355185826988b0b6abf38
point <alex.softx@gmail.com>
2013-06-24 12:28:14 +0300

    Update dragonfly config

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,19 +1,20 @@
 require 'dragonfly'
-module ImageMagick
-  module Utils
-    def identify(temp_object)
-      # example of details string:
-      # myimage.png PNG 200x100 200x100+0+0 8-bit DirectClass 31.2kb
-      format, width, height, depth = raw_identify(temp_object).scan(/([A-Z0-9]+) (\d+)x(\d+) .* (\d+)-bit/)[0]
-      {
-        :format => format.downcase.to_sym,
-        :width => width.to_i,
-        :height => height.to_i,
-        :depth => depth.to_i
-      }
-    end
-  end
-end
+#module ImageMagick
+  #module Utils
+    #private
+    #def identify(temp_object)
+       example of details string:
+       myimage.png PNG 200x100 200x100+0+0 8-bit DirectClass 31.2kb
+      #format, width, height, depth = raw_identify(temp_object).scan(/([A-Z0-9]+) (\d+)x(\d+) .* (\d+)-bit/)[0]
+      #{
+        #:format => format.downcase.to_sym,
+        #:width => width.to_i,
+        #:height => height.to_i,
+        #:depth => depth.to_i
+      #}
+    #end
+  #end
+#end
 ## initialize Dragonfly ##
 
 app = Dragonfly[:images]
@@ -26,8 +27,9 @@ Dragonfly[:images].configure do |c|
   # Convert absolute location needs to be specified
   # to avoid issues with Phusion Passenger not using $PATH
   convert = `which convert`.strip.presence || "/usr/local/bin/convert"
+  identify = `which identify`.strip.presence || /usr/bin/identify
   c.convert_command  = convert
-  c.identify_command = convert
+  c.identify_command = identify
 
   c.allow_fetch_url  = true
   c.allow_fetch_file = true
