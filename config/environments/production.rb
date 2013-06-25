3091b056325042fc0b018a922be66f6d2cbd6cd3
point <alex.softx@gmail.com>
2013-06-25 17:43:11 +0300

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,5 +61,14 @@ Myapp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'turumburum.com',
+    :user_name            => 'contact@turumburum.com',
+    :password             => 'turum69burum',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 
 end
